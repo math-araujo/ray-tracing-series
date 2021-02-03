958ddf254d4e1d15b0230791c61f6f8b8a7bd69f
M-A-rs <araujo_m_@hotmail.com>
2021-02-03 16:12:19 -0300

    Add gamma correction

--- src/common/color.hpp
+++ src/common/color.hpp
@@ -4,6 +4,7 @@
 #include "util.hpp"
 #include "vector3.hpp"
 
+#include <cmath>
 #include <iostream>
 
 void write_color(std::ostream& out, Color pixel_color, int samples_per_pixel)
@@ -13,9 +14,11 @@ void write_color(std::ostream& out, Color pixel_color, int samples_per_pixel)
     auto blue = pixel_color.z();
 
     auto scale = 1.0 / samples_per_pixel;
-    red *= scale;
-    green *= scale;
-    blue *= scale;
+    
+    // Gamma correction using gamma = 2.0
+    red = std::sqrt(scale * red);
+    green = std::sqrt(scale * green);
+    blue = std::sqrt(scale * blue);
     
     out << static_cast<int>(256 * clamp(red, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * clamp(green, 0.0, 0.999)) << ' '
