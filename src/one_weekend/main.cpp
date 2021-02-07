299e24f5997b4c6f92c36f038dff97e4635258bb
M-A-rs <araujo_m_@hotmail.com>
2021-02-07 02:19:31 -0300

    Add random scene

--- src/one_weekend/main.cpp
+++ src/one_weekend/main.cpp
@@ -9,30 +9,27 @@
 
 #include <iostream>
 
+// Simple scene developed along the book using lambertian, metal and dielectrics materials
+HittableList hollow_glass_scene();
+
+// Final render scene
+HittableList random_scene();
+
+// Recursive ray tracing function to compute color for a pixel
 Color ray_color(const Ray& ray, const Hittable& world, int depth);
 
 int main()
 {
     // Image settings
-    constexpr double aspect_ratio = 16.0 / 9.0;
-    constexpr int image_width = 400;
-    constexpr int image_height = static_cast<int>(image_width / aspect_ratio); // 225
-    constexpr int samples_per_pixel = 100;
+    constexpr double aspect_ratio = 3.0 / 2.0;
+    constexpr int image_width = 1200;
+    constexpr int image_height = static_cast<int>(image_width / aspect_ratio); // 800
+    constexpr int samples_per_pixel = 10;
     constexpr int max_depth = 50;
 
     // World
-    HittableList world;
-
-    auto material_ground = std::make_shared<Lambertian>(Color{0.8, 0.8, 0.0});
-    auto material_center = std::make_shared<Lambertian>(Color{0.1, 0.2, 0.5});
-    auto material_left = std::make_shared<Dielectric>(1.5);
-    auto material_right = std::make_shared<Metal>(Color{0.8, 0.6, 0.2}, 0.0);
-
-    world.add(std::make_shared<Sphere>(Point3{0.0, -100.5, -1.0}, 100.0, material_ground));
-    world.add(std::make_shared<Sphere>(Point3{0.0, 0.0, -1.0}, 0.5, material_center));
-    world.add(std::make_shared<Sphere>(Point3{-1.0, 0.0, -1.0}, 0.5, material_left));
-    world.add(std::make_shared<Sphere>(Point3{-1.0, 0.0, -1.0}, -0.45, material_left)); // Negative radius for hollow glass trick
-    world.add(std::make_shared<Sphere>(Point3{1.0, 0.0, -1.0}, 0.5, material_right));
+    //auto world = hollow_glass_scene();
+    auto world = random_scene();
 
     /*
     Wide-angle view world settings
@@ -44,12 +41,12 @@ int main()
     */
 
     // Camera; set distance_to_focus to 1.0 and aperture to 0.0 to remove depth of field/defocus blur
-    Point3 look_from{3, 3, 2};
-    Point3 look_at{0, 0, -1};
+    Point3 look_from{13, 2, 3};
+    Point3 look_at{0, 0, 0};
     Vector3 view_up{0, 1, 0};
     auto vertical_fov = 20.0;
-    auto distance_to_focus = (look_from - look_at).length();
-    auto aperture = 2.0;
+    auto distance_to_focus = 10.0;
+    auto aperture = 0.1;
     Camera camera{look_from, look_at, view_up, vertical_fov, aspect_ratio, aperture, distance_to_focus};
 
     // Render
@@ -77,6 +74,79 @@ int main()
     }
     
     std::cerr << "\nDone.\n";
+}
+
+HittableList hollow_glass_scene()
+{
+    HittableList world;
+
+    auto material_ground = std::make_shared<Lambertian>(Color{0.8, 0.8, 0.0});
+    auto material_center = std::make_shared<Lambertian>(Color{0.1, 0.2, 0.5});
+    auto material_left = std::make_shared<Dielectric>(1.5);
+    auto material_right = std::make_shared<Metal>(Color{0.8, 0.6, 0.2}, 0.0);
+
+    world.add(std::make_shared<Sphere>(Point3{0.0, -100.5, -1.0}, 100.0, material_ground));
+    world.add(std::make_shared<Sphere>(Point3{0.0, 0.0, -1.0}, 0.5, material_center));
+    world.add(std::make_shared<Sphere>(Point3{-1.0, 0.0, -1.0}, 0.5, material_left));
+    world.add(std::make_shared<Sphere>(Point3{-1.0, 0.0, -1.0}, -0.45, material_left)); // Negative radius for hollow glass trick
+    world.add(std::make_shared<Sphere>(Point3{1.0, 0.0, -1.0}, 0.5, material_right));
+
+    return world;
+}
+
+HittableList random_scene()
+{   
+    constexpr double diffuse_threshold = 0.8;
+    constexpr double metal_threshold = 0.95;
+
+    HittableList world;
+
+    auto ground_material = std::make_shared<Lambertian>(Color{0.5, 0.5, 0.5});
+    world.add(std::make_shared<Sphere>(Point3{0, -1000, 0}, 1000, ground_material));
+
+    for (int a = -11; a < 11; ++a)
+    {
+        for (int b = -11; b < 11; ++b)
+        {
+            Point3 center{a + 0.9 * random_double(), 0.2, b + 0.9 * random_double()};
+
+            if ((center - Point3{4, 0.2, 0}).length() > 0.9)
+            {
+                std::shared_ptr<Material> sphere_material;
+
+                auto choose_material = random_double();
+
+                if (choose_material < diffuse_threshold) // Choose diffuse material
+                {
+                    auto albedo = Color::random() * Color::random();
+                    sphere_material = std::make_shared<Lambertian>(albedo);
+                }
+                else if (choose_material < metal_threshold) // Choose metal material
+                {
+                    auto albedo = Color::random(0.5, 1);
+                    auto fuzziness = random_double(0, 0.5);
+                    sphere_material = std::make_shared<Metal>(albedo, fuzziness);
+                }
+                else // Choose dielectric material
+                {
+                    sphere_material = std::make_shared<Dielectric>(1.5);
+                }
+
+                world.add(std::make_shared<Sphere>(center, 0.2, sphere_material));
+            }
+        }
+    }
+
+    auto dielectric = std::make_shared<Dielectric>(1.5);
+    world.add(std::make_shared<Sphere>(Point3{0, 1, 0}, 1.0, dielectric));
+
+    auto lambertian = std::make_shared<Lambertian>(Color{0.4, 0.2, 0.1});
+    world.add(std::make_shared<Sphere>(Point3{-4, 1, 0}, 1.0, lambertian));
+
+    auto metal = std::make_shared<Metal>(Color{0.7, 0.6, 0.5}, 0.0);
+    world.add(std::make_shared<Sphere>(Point3{4, 1, 0}, 1.0, metal));
+
+    return world;
 }
 
 Color ray_color(const Ray& ray, const Hittable& world, int depth)
